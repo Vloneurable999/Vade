8e0a49b3d5aa2c172d9c13dba01609b6c55cb06c
Skyler <Vloneurable@gmail.com>
2025-01-16 11:24:11 -0600

    added highlights system

---
+++ src/client/Highlights.lua
@@ -0,0 +1,67 @@
+--// Variables \\--
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local LocalPlayer = Players.LocalPlayer
+local Roles = nil
+local Murderer = nil
+local Sheriff = nil
+local Hero = nil
+
+--// Functions \\--
+
+CreateHighlight = function()
+	for _, Player in pairs(Players:GetChildren()) do
+		if Player ~= LocalPlayer and Player.Character and not Player.Character:FindFirstChild("Highlight") then
+			Instance.new("Highlight", Player.Character)           
+		end
+	end
+end
+
+IsAlive = function(Player)
+	for PlayerName, Data in pairs(Roles) do
+		if Player.Name == PlayerName then
+			if not Data.Killed and not Data.Dead then
+				return true
+			else
+				return false
+			end
+		end
+	end
+end
+
+UpdateHighlights = function()
+	for _, Player in pairs(Players:GetChildren()) do
+		if Player ~= LocalPlayer and Player.Character and Player.Character:FindFirstChild("Highlight") then
+			local Highlight = Player.Character:FindFirstChild("Highlight")
+			
+			if Player.Name == Sheriff and IsAlive(Player) then
+				Highlight.FillColor = Color3.fromRGB(0, 0, 225)
+			elseif Player.Name == Murderer and IsAlive(Player) then
+				Highlight.FillColor = Color3.fromRGB(225, 0, 0)
+			elseif Player.Name == Hero and IsAlive(Player) and not IsAlive(game.Players[Sheriff]) then
+				Highlight.FillColor = Color3.fromRGB(255, 250, 0)
+			else
+				Highlight.FillColor = Color3.fromRGB(0, 225, 0)
+			end
+		end
+	end
+end
+
+RunService.RenderStepped:connect(function()
+	Roles = ReplicatedStorage:FindFirstChild("GetPlayerData", true):InvokeServer()
+	
+	for PlayerName, Data in pairs(Roles) do
+		if Data.Role == "Murderer" then
+			Murderer = PlayerName
+		elseif Data.Role == 'Sheriff'then
+			Sheriff = PlayerName
+		elseif Data.Role == 'Hero'then
+			Hero = PlayerName
+		end
+	end
+	
+	CreateHighlight()
+	UpdateHighlights()
+end)

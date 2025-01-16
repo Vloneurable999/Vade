9fdda6d8f85ef2a4b3898cfd579d51039dd275be
Skyler <Vloneurable@gmail.com>
2025-01-16 11:49:00 -0600

    added KeyWindow.lua

---
+++ src/UIs/Windows/KeyWindow.lua
@@ -0,0 +1,47 @@
+--// Variables \\--
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+--// Main \\--
+
+return function()
+	local Window = Rayfield:CreateWindow({
+		Name = "Vade Key",
+		LoadingTitle = "Vade",
+		LoadingSubtitle = "by skyler_wrld",
+		Theme = "Default",
+		DisableRayfieldPrompts = false,
+		DisableBuildWarnings = false, 
+		KeySystem = false,
+
+		ConfigurationSaving = {
+			Enabled = false,
+			FolderName = nil,
+			FileName = nil
+		},
+
+		Discord = {
+			Enabled = true,
+			Invite = "https://discord.gg/Dm2B5QNgyX",
+			RememberJoins = true 
+		}
+	})
+
+	local KeyTab = Window:CreateTab("Key")
+	
+	KeyTab:CreateInput({
+		Name = "Key Input",
+		CurrentValue = "",
+		PlaceholderText = "Enter Your Key",
+		RemoveTextAfterFocusLost = false,
+		Flag = "VadeKey",
+		
+		Callback = function(Text)
+			--Add Logic
+		end,
+	})
+
+	KeyTab:CreateButton({
+		Name = "Get Key",
+	})
+end

8c138296d7b147c456aa9738097dc0e48a7dfa41
Skyler <Vloneurable@gmail.com>
2025-01-16 12:29:41 -0600

    Update Vade.lua

--- src/Vade.lua
+++ src/Vade.lua
@@ -3,7 +3,7 @@ local Vade = {}
 function Vade:Load()
 	local KeyWindow = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/src/UIs/Windows/KeyWindow.lua"))()
 	
-	KeyWindow:Init()
+	KeyWindow.Init()
 end
 
 return Vade

c11438c75c74836f50a5584717a309da28eb8c37
Skyler <Vloneurable@gmail.com>
2025-01-16 11:59:24 -0600

    Updated Vade.lua

--- src/Vade.lua
+++ src/Vade.lua
@@ -1,6 +1,6 @@
 local Vade = {}
 
-Vade.Load = function()
+function Vade:Load()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/src/UIs/Windows/KeyWindow.lua?token=GHSAT0AAAAAAC5ODVV4S6XJGCRSLVCXHLVQZ4JI63Q'))()
 end
 

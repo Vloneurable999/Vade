90064e317e3bf5f1071618f0b645c9f3460f8a86
Skyler <Vloneurable@gmail.com>
2025-01-16 12:06:22 -0600

    some changes

--- src/Vade.lua
+++ src/Vade.lua
@@ -1,7 +1,7 @@
 local Vade = {}
 
 function Vade:Load()
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/src/UIs/Windows/KeyWindow.lua?token=GHSAT0AAAAAAC5ODVV4S6XJGCRSLVCXHLVQZ4JI63Q'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/src/UIs/Windows/KeyWindow.lua'))()
 end
 
 return Vade

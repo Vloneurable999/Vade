24e5f857aa637aeea3b9145e1107d3042a646974
Skyler <Vloneurable@gmail.com>
2025-01-17 12:35:15 -0600

    Update Directory.lua

--- Main/Directory.lua
+++ Main/Directory.lua
@@ -1,5 +1,20 @@
 local Module = {}
 
-Module.Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+-->> UI Lib <<--
+
+Module.Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
+-->> Windows <<--
+
+Module.KeyWindow = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/UIs/Windows/KeyWindow.lua"))()
+Module.MainWindow = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/UIs/Windows/MainWindow.lua"))()
+
+-->> Systems <<--
+
+Module.CheckConfig = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/Systems/CheckConfig.lua"))()
+Module.KeyValidation = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/Systems/KeyValidation.lua"))()
+Module.LoadVade = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/Systems/KeyValidation.lua"))()
+Module.Highlights = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/Systems/Highlights.lua"))()
+Module.Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/Systems/Notify.lua"))()
 
 return Module

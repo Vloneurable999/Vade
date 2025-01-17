f1bab4daa3bab1ce9bf0e7e7b42ade8a6a9b6958
Skyler <Vloneurable@gmail.com>
2025-01-17 12:19:40 -0600

    Update Vade.lua

--- Main/Vade.lua
+++ Main/Vade.lua
@@ -1,7 +1,9 @@
-local Vade = {}
+_G.Vade = {}
 
-function Vade.Load()
+_G.Vade.Directory = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/Directory.lua"))()
+
+function _G.Vade.Load()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/Vade/refs/heads/main/Main/UIs/Windows/KeyWindow.lua"))().Init()
 end
 
-return Vade
+return _G.Vade
